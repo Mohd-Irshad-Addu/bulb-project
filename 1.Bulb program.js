6ca74bf2dceee31becabdf3c9ccdd98f38f9bae8
unknown <irshadutkoori@gmail.com>
2021-12-25 11:51:14 +0530

    first commit

---
+++ 1.Bulb program.js
@@ -0,0 +1,14 @@
+//$("button").click(function(){
+  //  $("jumbotron").fadeout();
+//})
+
+$("#btn").click(function(){
+        $(".jumbotron").fadeOut()       
+})
+$("#btn").click(function(){
+    $("#btn").fadeOut()
+})
+$("#btn").click(function(){
+    $("p").text("Ooops you should not press that button, Now refresh the page")
+})
+
